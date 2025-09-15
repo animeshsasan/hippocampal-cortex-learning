286635d7c3cfd37e28d57691139f7a80efc3e778
Animesh Sasan <sasananimesh@gmail.com>
2025-09-15 13:08:27 +0530

    Add orthogonality loss and better testing

--- customLayer.py
+++ customLayer.py
@@ -1,26 +1,30 @@
 import torch
 import torch.nn as nn
-from torch.nn.functional import linear, softmax
+import torch.nn.utils.parametrizations as P
+from torch.nn.functional import linear
 from torch import Tensor
+import math
 
 
-class SparseCodedLayer(nn.Module):
+class CustomLayer(nn.Module):
     def __init__(self, 
                 in_features, 
                 out_features, 
-                out_features_allowed = None,
-                activation = None):
+                out_features_allowed = None):
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = nn.Parameter(
-            torch.randn((out_features, in_features)) * 0.01
-        )
-        self.bias = nn.Parameter(
-            torch.randn((out_features)) * 0.01
-        )
+
+        self.weight = nn.Parameter(torch.empty(out_features, in_features))
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5)) 
+
+        self.bias = nn.Parameter(torch.empty((out_features)))
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+        bound = 1 / math.sqrt(fan_in)
+        nn.init.uniform_(self.bias, -bound, bound)
+
         self.out_features_allowed = out_features_allowed
-        self.activation = activation
+        self.activation = nn.LeakyReLU(0.1)
         
 
     def forward(self, input: Tensor) -> Tensor:
